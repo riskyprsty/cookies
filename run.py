eda109633ded54de93b55dbe0eb5a6bf916c79a9
Risky Prsty <66415817+riskyprsty@users.noreply.github.com>
2020-09-21 11:52:09 +0700

    Update run.py

--- run.py
+++ run.py
@@ -22,7 +22,8 @@ def kukis():
 	if "c_user" in p.cookies.get_dict():
                 print "\n \033[1;91m[*] Result Cookies\n\n"
 		print(p.cookies.get_dict())
-                print "datr=" print(p.cookies['datr']) print";"
+                njir=p.cookies['datr']
+                print njir
                 print "\n \033[1;93m[*] Copy Cookies diatas"
                 sys.exit()
         if "checkpoint" in a:
